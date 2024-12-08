ee88fafc6799b67134ce054edd8e64620b1cff6f
work.kishan1 <work.kishan1@gmail.com>
2024-12-08 15:05:09 +0000

    Function to get fibbonacci of number

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ fn main() {
     // Since println!() has !, Its a macro not function 
     println!("Hello, world!");
     println!("{}", is_even(20));
+    println!("{}", fib(4));
+
+    let name = String::from("Kishan");
+    println!("The length of string is {}", get_str_len(name));
 }
 
 //Fucntion to check even number
@@ -15,3 +19,30 @@ fn is_even(num: i32) -> bool {
     return false;
 }
 
+//Function to write fibbonacci of number
+// 0 1 1 2 3 5 8
+fn fib(num: u32) -> u32 {
+    let mut first = 0;
+    let mut second = 1;
+
+    if num == 0 {
+        return first;
+    }
+
+    if num == 1 {
+        return second;
+    }
+
+    for _ in 0..(num - 1) {
+        let temp = second;
+        second = first + second;
+        first = temp;
+    }
+
+    return second;
+}
+
+//Function to get string length
+fn get_str_len(str: String) -> usize{
+    str.chars().count()  //Implicit return
+}
